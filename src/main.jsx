a3340c604dee8bc466cc7130c71a3e9c49ce9fb8
Rentoshiro <149387302+Rentoshiro@users.noreply.github.com>
2024-02-09 14:24:26 +0100

    deleted  ReactQueryDevtools

--- src/main.jsx
+++ src/main.jsx
@@ -11,9 +11,7 @@ const queryClient = new QueryClient();
 ReactDOM.createRoot(document.getElementById("root")).render(
   <AppProvider>
     <QueryClientProvider client={queryClient}>
-      <ReactQueryDevtools />
       <App />
-      <ReactQueryDevtools />
     </QueryClientProvider>
   </AppProvider>
 );
